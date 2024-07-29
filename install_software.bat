58195d1fee20ea7424f33240cc38a8a5ac923177
Kassia Oliveira <kassia.oliveira.silva@outlook.com>
2024-07-29 18:42:17 -0300

    Instalação Windows

---
+++ install_software.bat
@@ -0,0 +1,76 @@
+REM Instalação de Linguagens e Frameworks
+
+echo Instalando Java
+winget install -e --id Oracle.JDK.19
+
+echo Instalando Python
+winget install -e --id Python.Python.3.9
+
+echo Instalando Groovy
+winget install -e --id Apache.Groovy.4
+
+REM Instalação de Máquinas Virtuais
+
+echo Instalando VirtualBox
+winget install -e --id Oracle.VirtualBox
+
+REM Instalação de IDEs
+
+echo Instalando IntelliJ IDEA Community Edition
+winget install -e --id JetBrains.IntelliJIDEA.Community
+
+echo Instalando PyCharm Community Edition
+winget install -e --id JetBrains.PyCharm.Community
+
+
+REM Instalação de Ferramentas e Editores
+
+echo Instalando Windows Terminal
+winget install -e --id Microsoft.WindowsTerminal
+
+echo Instalando Git
+winget install -e --id Git.Git
+
+echo Instalando Notepad++
+winget install -e --id Notepad++.Notepad++
+
+echo Instalando Microsoft Visual Studio Code
+winget install -e --id Microsoft.VisualStudioCode
+
+echo Instalando DBeaver
+winget install -e --id dbeaver.dbeaver
+
+echo Instalando Postman
+winget install -e --id Postman.Postman
+
+echo Instalando Utilso
+winget install -e --id SerhiiSlieptsov.Utilso
+
+REM Instalação de Utilitários
+
+echo Instalando Discord
+winget install -e --id Discord.Discord
+
+echo Instalando Steam
+winget install -e --id Valve.Steam
+
+echo Instalando Epic Games Launcher
+winget install -e --id EpicGames.EpicGamesLauncher
+
+echo Instalando Spotify
+winget install -e --id Spotify.Spotify
+
+echo Instalando iCloud
+winget install -e --id Apple.iCloud
+
+echo Instalando Google Chrome
+winget install -e --id Google.Chrome
+
+echo Instalando WhatsApp
+winget install -e --id 9NKSQGP7F2NH
+
+echo Instalando MoveMouse
+winget install -e --id 9NSV16GT0WHR
+
+echo Instalando WinZip
+winget install -e --id Corel.WinZip
